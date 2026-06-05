a317eac8fa5659410fe6c691fddbedea93071cfd
Art Pelling <account@artpelling.de>
2026-06-05 20:23:47 +0200

    rename

--- src/acoular_ci/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-from pathlib import Path
-
-# Path to the ruff configuration file
-RUFF_CONFIG = Path(__file__).parent / "ruff.acoular.toml"
-
-warnings.warn('This package does not contain any code; only ruff.acoular.toml.')
